02f2aa3a33a60d3b70b7f73308dd8ffcf56f5c7e
Gabriel2019r <deltarune.ultimate.rp@gmail.com>
2022-08-20 13:05:10 +0200

    it better not break

--- source/StoryMenuState.hx
+++ source/StoryMenuState.hx
@@ -63,7 +63,7 @@ class StoryMenuState extends MusicBeatState
 		scoreText.setFormat("Comic Sans MS Bold", 32);
 
 		txtWeekTitle = new FlxText(FlxG.width * 0.7, 10, 0, "", 32);
-		txtWeekTitle.setFormat("Comic Sans MS Bold", 32, FlxColor.WHITE, RIGHT);
+		txtWeekTitle.setFormat(Paths.font("vcr.ttf"), 32, FlxColor.WHITE, RIGHT);
 		txtWeekTitle.alpha = 0.7;
 
 		var rankText:FlxText = new FlxText(0, 10);
